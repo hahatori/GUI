5eb5f2cb49ab769ec1322468d8338aeed27ec334
hahatori <56164812+hahatori@users.noreply.github.com>
2020-01-07 06:27:06 +0000

    Add files via upload

---
+++ data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 02:22:35 2020
+
+@author: toriliang
+"""
+
+import random
+
+f = open("data.html", "w")
+
+f.write("<HTML>\n<BODY>\n")
+f.write("<STYLE>\n")
+f.write("TD {border: 1px solid black; padding: 15px;}\n")
+f.write("TH {border: 1px solid black; padding: 15px;}\n")
+f.write("</STYLE>\n")
+f.write("<TABLE class=\'datatable\' id=\'yxz\'>\n")
+f.write("<TR><TH>y</TH><TH>x</TH><TH>z</TH></TR>\n")
+
+for i in range(100):
+	y = random.randint(0,99)
+	x = random.randint(0,99)
+	z = random.randint(0,255)
+	f.write("<TR><TD class=\'y\'>" + str(y) + "</TD>")
+	f.write("<TD class=\'x\'>" + str(x) + "</TD>")
+	f.write("<TD class=\'z\'>" + str(z) + "</TD></TR>\n")
+
+f.write("</TABLE>\n</BODY>\n</HTML>")
+
+f.close()
